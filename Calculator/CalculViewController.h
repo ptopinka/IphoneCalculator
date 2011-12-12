57131087fde6d117cae727d2306b160d7c4f2451
Pavel Topinka <ptopinka@gmail.com>
2011-12-12 21:27:46 +0100

    Initial Commit

---
+++ Calculator/CalculViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculViewController.h
+//  Calculator
+//
+//  Created by Pavel Topinka on 12/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculViewController : UIViewController
+
+@end
---
+++ Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Pavel Topinka on 12/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CalculAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CalculAppDelegate class]));
+    }
+}
